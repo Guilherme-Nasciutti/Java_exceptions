2583cc9dead02ba35668a0a25c270a6097b4b737
Guilherme-Nasciutti <guilhermenasciutti@hotmail.com>
2023-06-27 11:27:04 -0300

    Solução boa. Classe com tratamento de exceções, e codigo melhor formatado.

--- src/main/java/application/ProgramaReservas.java
+++ src/main/java/application/ProgramaReservas.java
@@ -1,6 +1,7 @@
 package application;
 
 import model.entites.Reservas;
+import model.exceptions.DomainException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -8,36 +9,36 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class ProgramaReservas {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
 
-        System.out.print("Numero do quarto: ");
-        int numeroQuarto = scanner.nextInt();
-        System.out.print("Data de entrada: ");
-        Date dataEntrada = formatoData.parse(scanner.next());
-        System.out.print("Data de saída: ");
-        Date dataSaida = formatoData.parse(scanner.next());
+        try {
+            System.out.print("Numero do quarto: ");
+            int numeroQuarto = scanner.nextInt();
+            System.out.print("Data de checkIn (DD/MM/YYYY): ");
+            Date checkIn = formatoData.parse(scanner.next());
+            System.out.print("Data de checkOut (DD/MM/YYYY): ");
+            Date checkOut = formatoData.parse(scanner.next());
 
-        if (!dataSaida.after(dataEntrada)) {
-            System.out.print("Erro na reserva: Data de saída precisa ser maior que a data de entrada!");
-        } else {
-            Reservas reservas = new Reservas(numeroQuarto, dataEntrada, dataSaida);
-            System.out.println("\nReserva: " + reservas);
+            Reservas reserva = new Reservas(numeroQuarto, checkIn, checkOut);
+            System.out.println("\nReserva: " + reserva);
 
             System.out.println("\nDigite os dados para alteração da reserva:");
-            System.out.print("Data de entrada: ");
-            dataEntrada = formatoData.parse(scanner.next());
-            System.out.print("Data de saída: ");
-            dataSaida = formatoData.parse(scanner.next());
+            System.out.print("Data de checkIn (DD/MM/YYYY): ");
+            checkIn = formatoData.parse(scanner.next());
+            System.out.print("Data de checkOut (DD/MM/YYYY): ");
+            checkOut = formatoData.parse(scanner.next());
 
-            String erro = reservas.atualizacaoReserva(dataEntrada, dataSaida);
-            if (erro != null) {
-                System.out.println("Erro na reserva: " + erro);
-            } else {
-                System.out.println("\nReservas: " + reservas);
-            }
+            reserva.atualizacaoReservas(checkIn, checkOut);
+            System.out.println("Reserva atualizada: " + reserva);
+        } catch (ParseException parseException) {
+            System.out.println("Formato de data inválido!");
+        } catch (DomainException domainException) {
+            System.out.println("Erro na reserva: " + domainException.getMessage());
+        } catch (RuntimeException runtimeException) {
+            System.out.println("Erro inesperado!");
         }
         scanner.close();
     }
--- src/main/java/model/entites/Reservas.java
+++ src/main/java/model/entites/Reservas.java
@@ -1,5 +1,7 @@
 package model.entites;
 
+import model.exceptions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -7,15 +9,22 @@ import java.util.concurrent.TimeUnit;
 public class Reservas {
 
     private Integer numeroQuarto;
-    private Date dataEntrada;
-    private Date dataSaida;
+    private Date checkIn;
+    private Date checkOut;
 
-    SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
+    private static SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
 
-    public Reservas(Integer numeroQuarto, Date dataEntrada, Date dataSaida) {
+    public Reservas(Integer numeroQuarto, Date checkIn, Date checkOut) {
+        Date agora = new Date();
+        if (checkIn.before(agora)) {
+            throw new DomainException("A data de checkIn precisa ser maior que a data atual!");
+        }
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("A data de checkout precisa ser posterior a data de checkIn!");
+        }
         this.numeroQuarto = numeroQuarto;
-        this.dataEntrada = dataEntrada;
-        this.dataSaida = dataSaida;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
     public Integer getNumeroQuarto() {
@@ -26,43 +35,45 @@ public class Reservas {
         this.numeroQuarto = numeroQuarto;
     }
 
-    public Date getDataEntrada() {
-        return dataEntrada;
+    public Date getCheckIn() {
+        return checkIn;
     }
 
-    public Date getDataSaida() {
-        return dataSaida;
+    public Date getCheckOut() {
+        return checkOut;
     }
 
-    public long duracao() {
-        long diferenca = dataSaida.getTime() - dataEntrada.getTime(); // Variavel diferença para calcular os milisecundos entre a saida e a entrada
-        return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS); // Usa a classe TimeUnit para converter em dias os milisecundos da variavel diferença
+    public Long duration() {
+        long diferenca = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS);
     }
 
-    public String atualizacaoReserva(Date dataEntrada, Date dataSaida) {
-
+    public void atualizacaoReservas(Date checkIn, Date checkOut) {
         Date agora = new Date();
-        if (dataEntrada.before(agora) || dataSaida.before(agora)) {
-            return "As datas para remarcação devem ser futuras às atuais!";
+        if (checkIn.before(agora)) {
+            throw new DomainException("A data de checkIn precisa ser maior que a data atual!");
         }
-        if (!dataSaida.after(dataEntrada)) {
-            return "Data de saída precisa ser maior que a data de entrada!";
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("A data de checkout precisa ser posterior a data de checkIn!");
         }
-        this.dataEntrada = dataEntrada;
-        this.dataSaida = dataSaida;
-        return null;
+        if (checkIn.before(agora) || checkOut.before(agora)) {
+            throw new DomainException("As datas para reservas precisam ser futuras a data atual!");
+        }
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
     @Override
     public String toString() {
         return "Quarto: "
                 + numeroQuarto
-                + ", Data de entrada: "
-                + formatoData.format(dataEntrada)
-                + ", Data de saída: "
-                + formatoData.format(dataSaida)
-                + ", "
-                + duracao()
+                + ", Data de checkIn: "
+                + formatoData.format(checkIn)
+                + ", Data de checkOut: "
+                + formatoData.format(checkOut)
+                + ", Duração: "
+                + duration()
                 + " noites.";
     }
+
 }
